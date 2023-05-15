c85ddf99a9ec5c00f674e22ee8d7024f6d2ddf72
Albert2311 <nguyetanh.23112002@gmail.com>
2023-05-16 00:45:11 +0700

    css

--- home.php
+++ home.php
@@ -2,19 +2,19 @@
 include_once("controller2.php"); 
 ?>
 <?php
-if ($_SERVER['REQUEST_METHOD'] != "POST") {
-    header("location: codeQR.php");
-    die();
-}
-$Authenticator = new Authenticator();
+// if ($_SERVER['REQUEST_METHOD'] != "POST") {
+//     header("location: codeQR.php");
+//     die();
+// }
+// $Authenticator = new Authenticator();
 
-$checkResult = $Authenticator->verifyCode($_SESSION['auth_secret'], $_POST['otp'], 3);    // 2 = 2*30sec clock tolerance
+// $checkResult = $Authenticator->verifyCode($_SESSION['auth_secret'], $_POST['otp'], 3);    // 2 = 2*30sec clock tolerance
 
-if (!$checkResult) {
-    $_SESSION['failed'] = true;
-    header("location: codeQR.php");
-    die();
-} 
+// if (!$checkResult) {
+//     $_SESSION['failed'] = true;
+//     header("location: codeQR.php");
+//     die();
+// } 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -25,26 +25,10 @@ if (!$checkResult) {
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
         <meta name="description" content="Implement Google like Time-Based Authentication into your existing PHP application. And learn How to Build it? How it Works? and Why is it Necessary these days."/>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-</head>
-<body>
-        <!-- welcome home
-        <a href="logout.php" class="header__navbar-icon-link">
-        <i class="fa-solid fa-arrow-right-from-bracket"></i>
-        <p>Đăng xuất</p></a> -->
-        
-</body>
-</html>
-
-<!DOCTYPE html>
-<html lang="en">
-<!--divinectorweb.com-->
-<head>
-	<meta charset="UTF-8">
-	<title>Home</title>
     <link rel="stylesheet" href="css/home.css">
 </head>
 <body>
-	<div class="menu-area">
+<div class="menu-area">
 		<a class="logo" href="#">Computer Network Security</a>
 		<ul class="nav">
 			<li>
@@ -64,11 +48,10 @@ if (!$checkResult) {
             <p>Trần Thái Nguyên - 46.01.104.126</p>
             <a href="logout.php"><button class="button-34" role="button">Đăng xuất</button></a>
 		</div>
-	</section>
-	
-    <a href="logout.php"><p>Đăng xuất</p></a>
-        
+	</section>   
 </body>
 </html>
+
+
 
 
